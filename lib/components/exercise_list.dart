453c58409b0177c7968ffd2511e78f2b2c79be2a
4lexrossi <devalexrossi@gmail.com>
2024-11-24 18:20:00 -0300

    feat: add fonts and refactor classes

--- lib/components/exercise_list.dart
+++ lib/components/exercise_list.dart
@@ -4,55 +4,60 @@ import 'package:gym_app/models/exercise.dart';
 class ExerciseList extends StatelessWidget {
   final List<Exercise> exercises;
 
-  ExerciseList(this.exercises);
+  const ExerciseList(this.exercises, {super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: exercises.map((ex) {
-        return Card(
-            child: Row(
-          children: [
-            Container(
-              margin: const EdgeInsets.symmetric(
-                horizontal: 15,
-                vertical: 10,
-              ),
-              decoration: BoxDecoration(
-                  border: Border.all(
-                    color: const Color.fromARGB(255, 1, 98, 177),
-                    width: 2,
-                  ),
-                  borderRadius: const BorderRadius.all(Radius.circular(5))),
-              padding: const EdgeInsets.all(10),
-              child: Text(
-                'Séries ${ex.set}',
-                style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                  color: Color.fromARGB(255, 1, 98, 177),
+    return SizedBox(
+      height: 330,
+      child: ListView.builder(
+        itemCount: exercises.length,
+        itemBuilder: (context, index) {
+          final ex = exercises[index];
+          return Card(
+              child: Row(
+            children: [
+              Container(
+                margin: const EdgeInsets.symmetric(
+                  horizontal: 15,
+                  vertical: 10,
                 ),
-              ),
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  ex.title,
+                decoration: BoxDecoration(
+                    border: Border.all(
+                      color: const Color.fromARGB(255, 1, 98, 177),
+                      width: 2,
+                    ),
+                    borderRadius: const BorderRadius.all(Radius.circular(5))),
+                padding: const EdgeInsets.all(10),
+                child: Text(
+                  'Sets: ${ex.set}',
                   style: const TextStyle(
-                    fontSize: 16,
                     fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    color: Color.fromARGB(255, 1, 98, 177),
                   ),
                 ),
-                Text(
-                  'Peso ${ex.weight.toStringAsFixed(2)}',
-                  style: const TextStyle(color: Colors.grey),
-                ),
-              ],
-            )
-          ],
-        ));
-      }).toList(),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    ex.title,
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    'Peso ${ex.weight.toStringAsFixed(2)}',
+                    style: const TextStyle(color: Colors.grey),
+                  ),
+                ],
+              )
+            ],
+          ));
+        },
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,116 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:gym_app/components/exercise_form.dart';
 import 'package:gym_app/components/exercise_list.dart';
-import 'package:gym_app/components/exercise_user.dart';
 import 'package:gym_app/models/exercise.dart';
 
-main() => runApp(const MyGym());
+main() => runApp(MyGym());
 
 class MyGym extends StatelessWidget {
   const MyGym({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: MyHomePage());
+    final ThemeData tema = ThemeData();
+
+    return MaterialApp(
+      home: MyHomePage(),
+      theme: tema.copyWith(
+        colorScheme: tema.colorScheme.copyWith(
+          primary: Colors.purple,
+          secondary: Colors.amber,
+        ),
+        textTheme: tema.textTheme.copyWith(
+          titleLarge: const TextStyle(
+            fontFamily: 'OpenSans',
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+        appBarTheme: const AppBarTheme(
+          titleTextStyle: TextStyle(
+            fontFamily: 'OpenSans',
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final _exercises = [
+    Exercise(id: 'E1', title: 'title1', set: 3, weight: 25.2),
+    Exercise(id: 'E2', title: 'title2', set: 4, weight: 15.4),
+  ];
+
+  _addExercise(String title, num set, double weight) {
+    final newExercise = Exercise(
+      id: Random().nextDouble().toString(),
+      title: title,
+      set: set,
+      weight: weight,
+    );
+
+    setState(() {
+      _exercises.add(newExercise);
+    });
+
+    Navigator.of(context).pop();
+  }
+
+  _openExerciseModal(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (_) {
+        return ExerciseForm(_addExercise);
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Personal Training'),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Container(
-            width: double.infinity,
-            child: const Card(
-              color: Colors.blue,
-              elevation: 5,
-              child: Text('Graph'),
-            ),
-          ),
-          ExerciseUser(),
+        actions: [
+          IconButton(
+            onPressed: () => _openExerciseModal(context),
+            icon: const Icon(Icons.add),
+          )
         ],
       ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            SizedBox(
+              width: double.infinity,
+              child: const Card(
+                color: Colors.blue,
+                elevation: 5,
+                child: Text('Graph'),
+              ),
+            ),
+            ExerciseList(_exercises),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () => _openExerciseModal(context),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
