a9fc1c7d62b3cd451129d0bd2ec2d3bb35aba044
4lexrossi <devalexrossi@gmail.com>
2024-11-24 22:10:14 -0300

    feat: add navigation botton bar

--- lib/components/exercise_list.dart
+++ lib/components/exercise_list.dart
@@ -3,65 +3,70 @@ import 'package:gym_app/models/exercise.dart';
 
 class ExerciseList extends StatelessWidget {
   final List<Exercise> exercises;
+  final void Function(String) onRemove;
 
-  const ExerciseList(this.exercises, {super.key});
+  const ExerciseList(this.exercises, this.onRemove, {super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 330,
-      child: exercises.isEmpty
-          ? Column(
-              children: [
-                const SizedBox(height: 20),
-                Text(
-                  'No exercise registred',
-                  style: Theme.of(context).textTheme.titleLarge,
+    return exercises.isEmpty
+        ? Column(
+            children: [
+              const SizedBox(height: 20),
+              Text(
+                'No exercise registered',
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+              const SizedBox(height: 20),
+              SizedBox(
+                height: 200,
+                child: Image.asset(
+                  'assets/images/waiting.png',
+                  fit: BoxFit.cover,
                 ),
-                const SizedBox(height: 20),
-                SizedBox(
-                  height: 200,
-                  child: Image.asset(
-                    'assets/images/waiting.png',
-                    fit: BoxFit.cover,
-                  ),
+              ),
+            ],
+          )
+        : ListView.builder(
+            itemCount: exercises.length,
+            itemBuilder: (context, index) {
+              final ex = exercises[index];
+              return Card(
+                elevation: 5,
+                margin: const EdgeInsets.symmetric(
+                  vertical: 8,
+                  horizontal: 5,
                 ),
-              ],
-            )
-          : ListView.builder(
-              itemCount: exercises.length,
-              itemBuilder: (context, index) {
-                final ex = exercises[index];
-                return Card(
-                  elevation: 5,
-                  margin: const EdgeInsets.symmetric(
-                    vertical: 8,
-                    horizontal: 5,
-                  ),
-                  child: ListTile(
-                    leading: CircleAvatar(
-                      radius: 30,
-                      child: Padding(
-                        padding: const EdgeInsets.all(6.0),
-                        child: FittedBox(
-                          child: Column(
-                            children: [
-                              const Text('Sets'),
-                              Text('${ex.set}'),
-                            ],
-                          ),
+                child: ListTile(
+                  leading: CircleAvatar(
+                    radius: 30,
+                    child: Padding(
+                      padding: const EdgeInsets.all(6.0),
+                      child: FittedBox(
+                        child: Column(
+                          children: [
+                            const Text('Sets'),
+                            Text('${ex.set}'),
+                          ],
                         ),
                       ),
                     ),
-                    title: Text(
-                      ex.title,
-                      style: Theme.of(context).textTheme.titleLarge,
-                    ),
-                    subtitle: Text('Weight: ${ex.weight.toStringAsFixed(2)}kg'),
                   ),
-                );
-              },
-            ),
-    );
+                  title: Text(
+                    ex.title,
+                    style: Theme.of(context).textTheme.titleLarge,
+                  ),
+                  subtitle: Text(
+                    'Weight: ${ex.weight.toStringAsFixed(2)}kg',
+                  ),
+                  trailing: IconButton(
+                    color: Colors.redAccent,
+                    onPressed: () => onRemove(ex.id),
+                    icon: const Icon(Icons.delete),
+                  ),
+                ),
+              );
+            },
+          );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,25 +3,26 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:gym_app/components/exercise_form.dart';
 import 'package:gym_app/components/exercise_list.dart';
+import 'package:gym_app/models/day_exercise.dart';
 import 'package:gym_app/models/exercise.dart';
 
-main() => runApp(MyGym());
+main() => runApp(const MyGym());
 
 class MyGym extends StatelessWidget {
   const MyGym({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final ThemeData tema = ThemeData();
+    final ThemeData customTheme = ThemeData();
 
     return MaterialApp(
-      home: MyHomePage(),
-      theme: tema.copyWith(
-        colorScheme: tema.colorScheme.copyWith(
+      home: const MyHomePage(),
+      theme: customTheme.copyWith(
+        colorScheme: customTheme.colorScheme.copyWith(
           primary: Colors.purple,
           secondary: Colors.amber,
         ),
-        textTheme: tema.textTheme.copyWith(
+        textTheme: customTheme.textTheme.copyWith(
           titleLarge: const TextStyle(
             fontFamily: 'OpenSans',
             fontSize: 18,
@@ -49,9 +50,25 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int _page = 0;
   final List<Exercise> _exercises = [
     Exercise(id: 'E1', title: 'title1', set: 3, weight: 25.2),
     Exercise(id: 'E2', title: 'title2', set: 4, weight: 15.4),
+    Exercise(id: 'E1', title: 'title1', set: 3, weight: 25.2),
+    Exercise(id: 'E2', title: 'title2', set: 4, weight: 15.4),
+    Exercise(id: 'E1', title: 'title1', set: 3, weight: 25.2),
+    Exercise(id: 'E2', title: 'title2', set: 4, weight: 15.4),
+    Exercise(id: 'E1', title: 'title1', set: 3, weight: 25.2),
+    Exercise(id: 'E2', title: 'title2', set: 4, weight: 15.4),
+    Exercise(id: 'E1', title: 'title1', set: 3, weight: 25.2),
+    Exercise(id: 'E2', title: 'title2', set: 4, weight: 15.4),
+  ];
+
+  final List<DayExercise> dayExercise = [
+    DayExercise(dayName: 'Monday', dayNumber: 1),
+    DayExercise(dayName: 'Tuesday', dayNumber: 2),
+    DayExercise(dayName: 'Wenesday', dayNumber: 3),
+    DayExercise(dayName: 'Friday', dayNumber: 4),
   ];
 
   _addExercise(String title, num set, double weight) {
@@ -62,11 +79,21 @@ class _MyHomePageState extends State<MyHomePage> {
       weight: weight,
     );
 
+    if (title.isEmpty || set <= 0) {
+      return;
+    }
+
     setState(() {
       _exercises.add(newExercise);
     });
 
     Navigator.of(context).pop();
+  }
+
+  _removeExercise(String id) {
+    setState(() {
+      _exercises.removeWhere((ex) => ex.id == id);
+    });
   }
 
   _openExerciseModal(BuildContext context) {
@@ -80,31 +107,98 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final appBar = AppBar(
+      title: const Text('Personal Training'),
+      actions: [
+        IconButton(
+          onPressed: () => _openExerciseModal(context),
+          icon: const Icon(Icons.add),
+        )
+      ],
+    );
+
+    final availableHeight = MediaQuery.of(context).size.height -
+        appBar.preferredSize.height -
+        MediaQuery.of(context).padding.top;
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Personal Training'),
-        actions: [
-          IconButton(
-            onPressed: () => _openExerciseModal(context),
-            icon: const Icon(Icons.add),
-          )
+      appBar: appBar,
+      bottomNavigationBar: NavigationBar(
+        destinations: [
+          NavigationDestination(
+            icon: const Icon(Icons.fitness_center_outlined),
+            label: dayExercise[0].dayName,
+          ),
+          NavigationDestination(
+            icon: const Icon(Icons.fitness_center_outlined),
+            label: dayExercise[1].dayName,
+          ),
+          NavigationDestination(
+            icon: const Icon(Icons.fitness_center_outlined),
+            label: dayExercise[2].dayName,
+          ),
+          NavigationDestination(
+            icon: const Icon(Icons.fitness_center_outlined),
+            label: dayExercise[3].dayName,
+          ),
         ],
+        onDestinationSelected: (int index) {
+          setState(() {
+            _page = index;
+          });
+        },
+        animationDuration: const Duration(milliseconds: 100),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            const SizedBox(
-              width: double.infinity,
-              child: Card(
-                color: Colors.blue,
-                elevation: 5,
-                child: Text('Graph'),
-              ),
+      body: Column(
+        children: [
+          SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                if (_page == 0)
+                  Column(
+                    children: [
+                      Text(dayExercise[0].dayName),
+                      SizedBox(
+                        height: availableHeight * 0.85,
+                        child: ExerciseList(_exercises, _removeExercise),
+                      ),
+                    ],
+                  ),
+                if (_page == 1)
+                  Column(
+                    children: [
+                      Text(dayExercise[1].dayName),
+                      SizedBox(
+                        height: availableHeight * 0.85,
+                        child: ExerciseList(_exercises, _removeExercise),
+                      ),
+                    ],
+                  ),
+                if (_page == 2)
+                  Column(
+                    children: [
+                      Text(dayExercise[2].dayName),
+                      SizedBox(
+                        height: availableHeight * 0.85,
+                        child: ExerciseList(_exercises, _removeExercise),
+                      ),
+                    ],
+                  ),
+                if (_page == 3)
+                  Column(
+                    children: [
+                      Text(dayExercise[3].dayName),
+                      SizedBox(
+                        height: availableHeight * 0.85,
+                        child: ExerciseList(_exercises, _removeExercise),
+                      ),
+                    ],
+                  ),
+              ],
             ),
-            ExerciseList(_exercises),
-          ],
-        ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
